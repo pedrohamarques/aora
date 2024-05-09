772c391b0be7f0ea0cbfeb1aff316519bfde3632
Pedro Almeida <henrique.pedro.1992@gmail.com>
2024-05-09 17:50:10 -0300

    chore: fix styles in sign-up screen

--- src/screens/sign-up/sign-up.tsx
+++ src/screens/sign-up/sign-up.tsx
@@ -1,4 +1,12 @@
-import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
+import {
+    Image,
+    KeyboardAvoidingView,
+    Platform,
+    ScrollView,
+    Text,
+    TouchableOpacity,
+    View,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Feather } from "@expo/vector-icons";
 
@@ -19,79 +27,84 @@ export function SignUpScreen() {
     } = useSignUpScreen();
     return (
         <SafeAreaView className='bg-primary h-full'>
-            <ScrollView>
-                <View className='w-full justify-center h-full px-4 my-6'>
-                    <Image
-                        source={require("@assets/images/logo.png")}
-                        className='w-[115px] h-[35px]'
-                        resizeMode='contain'
-                    />
+            <KeyboardAvoidingView
+                behavior={Platform.OS === "ios" ? "padding" : "height"}
+                keyboardVerticalOffset={120}
+                className='flex-1'>
+                <ScrollView className='overflow-visible'>
+                    <View className='w-full justify-center h-full px-4 my-6'>
+                        <Image
+                            source={require("@assets/images/logo.png")}
+                            className='w-[115px] h-[35px]'
+                            resizeMode='contain'
+                        />
 
-                    <Text className='text-2xl text-white font-psemibold mt-10 '>
-                        Sign Up to Aora
-                    </Text>
-
-                    <FormField
-                        title='Username'
-                        value={form.username}
-                        onChangeText={username =>
-                            setForm({ ...form, username })
-                        }
-                        otherStyles='mt-7'
-                        testID='screens.sign-up.form-field.username'
-                        autoCapitalize='none'
-                        autoCorrect={false}
-                    />
-
-                    <FormField
-                        title='Email'
-                        value={form.email}
-                        onChangeText={email => setForm({ ...form, email })}
-                        otherStyles='mt-7'
-                        keyboardType='email-address'
-                        testID='screens.sign-up.form-field.email'
-                        autoCapitalize='none'
-                        autoCorrect={false}
-                    />
-                    <FormField
-                        title='Password'
-                        value={form.password}
-                        onChangeText={password =>
-                            setForm({ ...form, password })
-                        }
-                        otherStyles='mt-7'
-                        secureTextEntry={!showPassword}
-                        testID='screens.sign-up.form-field.password'>
-                        <TouchableOpacity
-                            onPress={() => setShowPassword(!showPassword)}>
-                            <Feather
-                                name={`${!showPassword ? "eye" : "eye-off"}`}
-                                size={24}
-                                color={"gray"}
-                            />
-                        </TouchableOpacity>
-                    </FormField>
-
-                    <CustomButton
-                        testID='screens.sign-up.custom-button'
-                        title='Sign Up'
-                        onPress={handleSignUp}
-                        containerStyles='mt-7'
-                        isLoading={isSubmitting}
-                    />
-
-                    <View className='justify-center pt-5 flex-row gap-2'>
-                        <Text className='text-lg text-gray-100 font-pregular'>
-                            Already have an account?
+                        <Text className='text-2xl text-white font-psemibold mt-10 '>
+                            Sign Up to Aora
                         </Text>
-                        <TouchableOpacity onPress={handleSignInPress}>
-                            <Text className='text-lg font-psemibold text-secondary'>
-                                Sign In
+
+                        <FormField
+                            title='Username'
+                            value={form.username}
+                            onChangeText={username =>
+                                setForm({ ...form, username })
+                            }
+                            otherStyles='mt-7'
+                            testID='screens.sign-up.form-field.username'
+                            autoCapitalize='none'
+                            autoCorrect={false}
+                        />
+
+                        <FormField
+                            title='Email'
+                            value={form.email}
+                            onChangeText={email => setForm({ ...form, email })}
+                            otherStyles='mt-7'
+                            keyboardType='email-address'
+                            testID='screens.sign-up.form-field.email'
+                            autoCapitalize='none'
+                            autoCorrect={false}
+                        />
+                        <FormField
+                            title='Password'
+                            value={form.password}
+                            onChangeText={password =>
+                                setForm({ ...form, password })
+                            }
+                            otherStyles='mt-7'
+                            secureTextEntry={!showPassword}
+                            testID='screens.sign-up.form-field.password'>
+                            <TouchableOpacity
+                                onPress={() => setShowPassword(!showPassword)}>
+                                <Feather
+                                    name={`${!showPassword ? "eye" : "eye-off"}`}
+                                    size={24}
+                                    color={"gray"}
+                                />
+                            </TouchableOpacity>
+                        </FormField>
+
+                        <CustomButton
+                            testID='screens.sign-up.custom-button'
+                            title='Sign Up'
+                            onPress={handleSignUp}
+                            containerStyles='mt-7'
+                            isLoading={isSubmitting}
+                        />
+
+                        <View className='justify-center pt-5 flex-row gap-2'>
+                            <Text className='text-lg text-gray-100 font-pregular'>
+                                Already have an account?
                             </Text>
-                        </TouchableOpacity>
+                            <TouchableOpacity onPress={handleSignInPress}>
+                                <Text className='text-lg font-psemibold text-secondary'>
+                                    Sign In
+                                </Text>
+                            </TouchableOpacity>
+                        </View>
                     </View>
-                </View>
-            </ScrollView>
+                </ScrollView>
+            </KeyboardAvoidingView>
         </SafeAreaView>
     );
 }
