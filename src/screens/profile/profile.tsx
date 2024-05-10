3987c2912c9fc14e1bd3c5b10d01b0f1ee453a70
Pedro Almeida <henrique.pedro.1992@gmail.com>
2024-05-10 14:51:27 -0300

    feat: add logout button in profile screen

--- src/screens/profile/profile.tsx
+++ src/screens/profile/profile.tsx
@@ -1,9 +1,11 @@
-import { Text, View } from "react-native";
+import { supabase } from "@services/supabase";
+import { Button, Text, View } from "react-native";
 
 export function ProfileScreen() {
     return (
-        <View>
+        <View className='flex-1 justify-center items-center'>
             <Text>Profile Screen</Text>
+            <Button onPress={() => supabase.auth.signOut()} title='Logout' />
         </View>
     );
 }
