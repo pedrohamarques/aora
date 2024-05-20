4c13f0fba897c394e2ea8862da1d8403707ca722
Pedro Almeida <henrique.pedro.1992@gmail.com>
2024-05-20 11:04:36 -0300

    feat: add lint stage to project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,10 +3,12 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
 
-
 export default [
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReactConfig,
+    {
+        settings: { react: { version: "detect" } },
+        languageOptions: { globals: { ...globals.browser, ...globals.node } },
+    },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    pluginReactConfig,
 ];
